d1d9aeb7a613db65ef211b2e284b03d586f803b5
ShashankBiplav <Biplavshashank7@gmail.com>
2020-05-26 16:37:02 +0530

    Added the Navigator to each item of categories

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/categorymeals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -6,18 +7,29 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem({this.title, this.colorBk});
 
+  void selectCategory(BuildContext ctx){
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_){
+      return CategoryMealsScreen();
+    },));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      child: Text(title),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [colorBk.withOpacity(0.7), colorBk],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+          onTap: ()=>selectCategory(context) ,
+          splashColor: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.circular(15),
+          child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Text(title, style: Theme.of(context).textTheme.title,),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [colorBk.withOpacity(0.7), colorBk],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(10),
         ),
-        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
