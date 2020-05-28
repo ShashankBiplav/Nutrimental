dee640e28fd60b81ede23ae4e448c7d31266ee10
ShashankBiplav <Biplavshashank7@gmail.com>
2020-05-28 14:41:42 +0530

    Added the navigation drawer

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget buildListTile({String title, IconData iconData}){
+    return ListTile(
+          leading: Icon(
+            iconData,
+            size: 26,
+          ),
+          title: Text(
+            title,
+            style: TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontSize: 24,
+                fontWeight: FontWeight.bold),
+          ),
+          onTap: () {},
+        );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: Column(
+      children: <Widget>[
+        Container(
+          width: double.infinity,
+          height: 120,
+          padding: EdgeInsets.all(20),
+          alignment: Alignment.centerLeft,
+          color: Theme.of(context)
+              .accentColor, // this property can only be used if BoxDecortion is not used else flutter throws an error
+          child: Text(
+            'Cooking Up !',
+            style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).primaryColor),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        buildListTile(title:'Meals' ,iconData: Icons.restaurant,),
+        buildListTile(title:'Filters' ,iconData: Icons.settings,),
+      ],
+    ));
+  }
+}
